a837fd10ea4c84ac3fc90c61fe7a0e88341e8ac7
ZongZhaobin <57524585+Big-voss@users.noreply.github.com>
2022-09-20 19:31:58 +0800

    Add files via upload

---
+++ spring_helloworld/src/main/java/com/zongzhaobin/spring/pojo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zongzhaobin.spring.pojo;
+
+/**
+ * @author zzb
+ * @version V1.0
+ * @creat 2022 0914 15:31
+ */
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("Hello,Spring");
+    }
+}
---
+++ spring_helloworld/src/test/java/com/zongzhaobin/spring/test/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.zongzhaobin.spring.test;
+
+import com.zongzhaobin.spring.pojo.HelloWorld;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zzb
+ * @version V1.0
+ * @creat 2022 0914 15:40
+ */
+public class HelloWorldTest {
+    @Test
+    public void test(){
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld helloworld = (HelloWorld) ioc.getBean("helloworld");
+        helloworld.sayHello();
+    }
+}
